38fb5d35372d3437c2cf00fd0e486f761cd6af2d
João Antonio Santana <joaosantana@outlook.com>
2021-02-13 16:38:15 -0300

    chore: commit inicial

---
+++ UnoDosTres/UnoDosTres.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace UnoDosTres.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new UnoDosTres.App(), args);
+            host.Run();
+        }
+    }
+}
